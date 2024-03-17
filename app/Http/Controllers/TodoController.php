ae1b9061705bc53f21ccd1c0f1b80efc2bf86bab
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-17 19:45:32 +0700

    env, mvc, route, relationship

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    
+    public function index()
+    {
+        return view('todo.index');
+    }
+
+
+    public function create()
+    {
+        return view('todo.create');
+    }
+
+
+    public function edit()
+    {
+        return view('todo.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
