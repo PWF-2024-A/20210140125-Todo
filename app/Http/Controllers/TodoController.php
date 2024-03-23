15ce001a5cef85065b908818fe22452182e0c47a
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-23 21:03:31 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    
+
     public function index()
     {
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
+
+        dd($todos);
         return view('todo.index');
     }
 
